ff7934db3be510fe736dd49476d73cb54006f3ce
cgalau97 <63465265+cgalau97@users.noreply.github.com>
2023-12-09 19:30:31 +0100

    Create nnUNetTrainerCustom.py

---
+++ nnunetv2/training/nnUNetTrainer/variants/CUSTOM/nnUNetTrainerCustom.py
@@ -0,0 +1,323 @@
+"""
+This module contains the custom nnUNetTrainer classes we have used.
+This script was developed by Ander Elkoroaristizabal. The code can be found at:
+https://github.com/ander-elkoroaristizabal/nnunet-ms-segmentation
+"""
+from os.path import join
+from typing import Optional, Dict, cast, Tuple
+
+import torch
+
+from nnunetv2.training.dataloading.data_loader_2d import nnUNetDataLoader2D
+from nnunetv2.training.dataloading.data_loader_3d import MinorityClassOversampling_nnUNetDataLoader3D
+from nnunetv2.training.nnUNetTrainer.nnUNetTrainer import nnUNetTrainer
+from nnunetv2.training.nnUNetTrainer.variants.sampling.nnUNetTrainer_probabilisticOversampling import \
+    nnUNetTrainer_probabilisticOversampling
+
+
+class EarlyStopping:
+    """EarlyStopping handler can be used to stop the training if no improvement after a given number of events.
+
+    Based on the PyTorch Ignite implementation.
+
+    Args:
+        patience: Number of events to wait if no improvement and then stop the training.
+        min_delta: A minimum increase in the score to qualify as an improvement,
+            i.e. an increase of less than or equal to `min_delta`, will count as no improvement.
+        cumulative_delta: It True, `min_delta` defines an increase since the last `patience` reset, otherwise,
+            it defines an increase after the last event. Default value is False.
+    """
+
+    _state_dict_all_req_keys = (
+        "counter",
+        "best_score",
+    )
+
+    def __init__(
+            self,
+            patience: int,
+            logger,
+            min_delta: float = 0.0,
+            cumulative_delta: bool = False,
+    ):
+
+        if patience < 1:
+            raise ValueError("Argument patience should be positive integer.")
+
+        if min_delta < 0.0:
+            raise ValueError("Argument min_delta should not be a negative number.")
+
+        self.patience = patience
+        self.min_delta = min_delta
+        self.cumulative_delta = cumulative_delta
+        self.counter = 0
+        self.best_score: Optional[float] = None
+        self.logger = logger
+
+    def stop_training(self, new_score: float) -> bool:
+        # First epoch:
+        if self.best_score is None:
+            self.best_score = new_score
+            return False
+        # If new score is not good enough:
+        elif new_score <= self.best_score + self.min_delta:
+            if not self.cumulative_delta and new_score > self.best_score:
+                self.best_score = new_score
+            self.counter += 1
+            print("EarlyStopping: %i / %i" % (self.counter, self.patience))
+            # Checking patience:
+            if self.counter >= self.patience:
+                return True
+            else:
+                return False
+        # If it is good enough:
+        else:
+            self.best_score = new_score
+            self.counter = 0
+            return False
+
+    def state_dict(self) -> Dict[str, float]:
+        """Method returns state dict with ``counter`` and ``best_score``.
+        Can be used to save internal state of the class.
+        """
+        return {"counter": self.counter, "best_score": cast(float, self.best_score)}
+
+    def load_state_dict(self, state_dict: Dict[str, float]) -> None:
+        """Method replace internal state of the class with provided state dict data.
+
+        Args:
+            state_dict: a dict with "counter" and "best_score" keys/values.
+        """
+        self.counter = state_dict["counter"]
+        self.best_score = state_dict["best_score"]
+
+
+class nnUNetTrainerEarlyStopping(nnUNetTrainer):
+    """
+    Variant of the nnU-Net using early stopping.
+    """
+
+    def __init__(
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device('cuda')
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.patience = 30
+        self.min_delta = 0
+        self.cumulative_delta = False
+        self.early_stopping = EarlyStopping(
+            patience=self.patience,
+            logger=self.logger,
+            min_delta=self.min_delta,
+            cumulative_delta=self.cumulative_delta
+        )
+        self.print_to_log_file("Using early stopping with patience:", self.patience)
+
+    def run_training(self):
+        self.on_train_start()
+
+        for epoch in range(self.current_epoch, self.num_epochs):
+            self.on_epoch_start()
+
+            self.on_train_epoch_start()
+            train_outputs = []
+            for batch_id in range(self.num_iterations_per_epoch):
+                train_outputs.append(self.train_step(next(self.dataloader_train)))
+            self.on_train_epoch_end(train_outputs)
+
+            with torch.no_grad():
+                self.on_validation_epoch_start()
+                val_outputs = []
+                for batch_id in range(self.num_val_iterations_per_epoch):
+                    val_outputs.append(self.validation_step(next(self.dataloader_val)))
+                self.on_validation_epoch_end(val_outputs)
+
+            self.on_epoch_end()
+            if self.early_stopping.stop_training(new_score=self.logger.my_fantastic_logging['ema_fg_dice'][-1]):
+                self.print_to_log_file("EarlyStopping: Stop training")
+                break
+            else:
+                if (self.early_stopping.counter > 0) & (self.early_stopping.counter % 10 == 0):
+                    self.save_checkpoint(join(self.output_folder, "checkpoint_latest.pth"))
+
+        self.on_train_end()
+
+
+class nnUNetTrainerHalfOversamplingEarlyStopping(nnUNetTrainerEarlyStopping, nnUNetTrainer_probabilisticOversampling):
+    """
+    Variant of the nnU-Net using early stopping and a 50% of oversampling in each batch.
+    """
+
+    def __init__(
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device('cuda')
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.oversample_foreground_percent = 0.5
+        self.print_to_log_file("Final oversample percent:", self.oversample_foreground_percent)
+
+
+class nnUNetTrainerFullOversamplingEarlyStopping(nnUNetTrainerEarlyStopping, nnUNetTrainer_probabilisticOversampling):
+    """
+    Variant of the nnU-Net using early stopping and a 100% of oversampling in each batch.
+    """
+
+    def __init__(
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device('cuda')
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.oversample_foreground_percent = 1
+        self.print_to_log_file("Final oversample percent:", self.oversample_foreground_percent)
+
+
+class nnUNetTrainerExtremeOversamplingEarlyStopping(nnUNetTrainerFullOversamplingEarlyStopping):
+    """
+    Variant of the nnU-Net using early stopping and a 100% of oversampling of new or evolving lesions in each batch.
+    """
+
+    def __init__(
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device('cuda')
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.print_to_log_file("Using minority class oversampling.")
+
+    def get_plain_dataloaders(self, initial_patch_size: Tuple[int, ...], dim: int):
+        dataset_tr, dataset_val = self.get_tr_and_val_datasets()
+
+        if dim == 2:
+            dl_tr = nnUNetDataLoader2D(dataset_tr, self.batch_size,
+                                       initial_patch_size,
+                                       self.configuration_manager.patch_size,
+                                       self.label_manager,
+                                       oversample_foreground_percent=self.oversample_foreground_percent,
+                                       sampling_probabilities=None, pad_sides=None)
+            dl_val = nnUNetDataLoader2D(dataset_val, self.batch_size,
+                                        self.configuration_manager.patch_size,
+                                        self.configuration_manager.patch_size,
+                                        self.label_manager,
+                                        oversample_foreground_percent=self.oversample_foreground_percent,
+                                        sampling_probabilities=None, pad_sides=None)
+        else:
+            # TODO Ander 26/4/23: Maybe shuffle=True?
+            dl_tr = MinorityClassOversampling_nnUNetDataLoader3D(
+                dataset_tr, self.batch_size,
+                initial_patch_size,
+                self.configuration_manager.patch_size,
+                self.label_manager,
+                oversample_foreground_percent=self.oversample_foreground_percent,
+                sampling_probabilities=None, pad_sides=None)
+            dl_val = MinorityClassOversampling_nnUNetDataLoader3D(
+                dataset_val, self.batch_size,
+                self.configuration_manager.patch_size,
+                self.configuration_manager.patch_size,
+                self.label_manager,
+                oversample_foreground_percent=self.oversample_foreground_percent,
+                sampling_probabilities=None, pad_sides=None)
+        return dl_tr, dl_val
+
+
+class nnUNetTrainerExtremeOversamplingEarlyStoppingLowLR(nnUNetTrainerExtremeOversamplingEarlyStopping):
+    """
+    Variant of the nnU-Net using early stopping, a 100% of oversampling of new or evolving lesions in each batch
+    and a lower Learning Rate.
+    """
+
+    def __init__(
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device('cuda')
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.initial_lr = 1e-3
+        self.print_to_log_file("Initial lr:", self.initial_lr)
+
+
+class nnUNetTrainerExtremeOversamplingEarlyStoppingVeryLowLR(nnUNetTrainerExtremeOversamplingEarlyStopping):
+    """
+    Variant of the nnU-Net using early stopping, a 100% of oversampling of new or evolving lesions in each batch
+    and a very low Learning Rate.
+    """
+
+    def __init__(
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device('cuda')
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.initial_lr = 5e-4
+        self.print_to_log_file("Initial lr:", self.initial_lr)
+
+
+class nnUNetTrainerExtremeOversamplingEarlyStoppingVeryLowLRVeryHighDecay(
+    nnUNetTrainerExtremeOversamplingEarlyStopping):
+    """
+    Variant of the nnU-Net using early stopping, a 100% of oversampling of new or evolving lesions in each batch,
+    a very low Learning Rate and a very high decay of the learning rate.
+    """
+
+    def __init__(
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device('cuda')
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.initial_lr = 5e-4
+        self.print_to_log_file("Initial lr:", self.initial_lr)
+        self.weight_decay = 5e-4
+        self.print_to_log_file("Weight decay:", self.weight_decay)
+
+
+class nnUNetTrainerExtremeOversamplingEarlyStoppingLowLRHigherDecay(nnUNetTrainerExtremeOversamplingEarlyStopping):
+    """
+    Variant of the nnU-Net using early stopping, a 100% of oversampling of new or evolving lesions in each batch,
+    a very low Learning Rate and a higher decay of the learning rate.
+    """
+
+    def __init__(
+            self,
+            plans: dict,
+            configuration: str,
+            fold: int,
+            dataset_json: dict,
+            unpack_dataset: bool = True,
+            device: torch.device = torch.device('cuda')
+    ):
+        super().__init__(plans, configuration, fold, dataset_json, unpack_dataset, device)
+        self.initial_lr = 1e-3
+        self.print_to_log_file("Initial lr:", self.initial_lr)
+        self.weight_decay = 1e-4
+        self.print_to_log_file("Weight decay:", self.weight_decay)
